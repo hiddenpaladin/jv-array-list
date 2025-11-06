b7038dffe38730ac382ed7e6d370e1212d1bb50a
Oleksandr Hordienko <code57231@gmail.com>
2025-11-06 23:34:20 +0200

    all test complete)

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,140 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
 public class ArrayList<T> implements List<T> {
+    static final int DEFAULT_ARRAY_SIZE = 10;
+    private Object[] elementData = new Object[DEFAULT_ARRAY_SIZE];
+    private int size = 0;
+
+    public Object[] getElementData() {
+        return elementData;
+    }
+
+    public void setElementData(Object[] elementData) {
+        this.elementData = elementData;
+    }
+
+    public Object[] grow() {
+        Object[] newArray = new Object[elementData.length + elementData.length / 2];
+        for (int i = 0; i < elementData.length; i++) {
+            newArray[i] = elementData[i];
+        }
+        return newArray;
+    }
+
     @Override
     public void add(T value) {
-
+        if (size == elementData.length - 1) {
+            elementData = grow();
+        }
+        elementData[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (size == elementData.length - 1) {
+            elementData = grow();
+        }
+        if (index > size || index < 0) {
+            throw new ArrayListIndexOutOfBoundsException("Index is invalid");
+        } else {
+            Object[] newArray = new Object[elementData.length];
+            for (int i = 0; i < index; i++) {
+                newArray[i] = elementData[i];
+            }
+            newArray[index] = value;
+            for (int i = index; i < elementData.length - 1; i++) {
+                newArray[i + 1] = elementData[i];
+            }
+            elementData = newArray;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        do {
+            elementData = grow();
+        } while (size + list.size() > elementData.length);
+        for (int i = 0; i < list.size(); i++) {
+            elementData[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (index < size && index >= 0) {
+            return (T) elementData[index];
+        } else {
+            throw new ArrayListIndexOutOfBoundsException("index is invalid");
+        }
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (index < size && index >= 0) {
+            elementData[index] = value;
+        } else {
+            throw new ArrayListIndexOutOfBoundsException("index is invalid");
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (index < size && index >= 0) {
+            Object[] newArray = new Object[elementData.length];
+            for (int i = 0; i < index; i++) {
+                newArray[i] = elementData[i];
+            }
+            for (int i = index; i < elementData.length - 1;i++) {
+                newArray[i] = elementData[i + 1];
+            }
+            Object removed = elementData[index];
+            elementData = newArray;
+            size--;
+            return (T)removed;
+        } else {
+            throw new ArrayListIndexOutOfBoundsException("index is invalid");
+        }
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        int index = -1;
+        for (int i = 0; i < elementData.length; i++) {
+            if (Objects.equals(elementData[i],element)) {
+                index = i;
+                break;
+            }
+        }
+        if (index != -1) {
+            Object[] newArray = new Object[elementData.length];
+            for (int i = 0; i < index; i++) {
+                newArray[i] = elementData[i];
+            }
+            for (int i = index; i < elementData.length - 1;i++) {
+                newArray[i] = elementData[i + 1];
+            }
+            Object removed = elementData[index];
+            elementData = newArray;
+            size--;
+            return (T)removed;
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0 ? true : false;
     }
 }
